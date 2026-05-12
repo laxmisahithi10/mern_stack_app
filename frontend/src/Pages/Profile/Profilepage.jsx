f77d5202098618aeae8af16d76d4649bb473a9fa
Sahithi <2410030043@klh.edu.in>
2026-05-12 15:46:17 +0530

    added profile page

---
+++ frontend/src/Pages/Profile/Profilepage.jsx
@@ -0,0 +1,56 @@
+// import { set } from 'mongoose';
+import React from 'react'
+import Service from '../../utils/http';
+import { Container } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { Avatar, Text, Stack, Loader} from '@mantine/core';
+
+export const Profilepage = () => {
+    const service = new Service();
+    const[loading, setLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    
+    const fetchUser = async () => {
+        try{
+            const response = await service.get("user/me");
+            setUser(response);
+            console.log(user);
+        } 
+        catch (error) {
+            console.error("User data unavailable", error);
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    if(loading){
+        return <Loader color="blue" />;
+    }
+    if(!user){
+        return <div>User Not found</div>
+    }
+
+    return(
+        <div>
+        <Container>
+            <Stack
+            h={300}
+            bg="var(--mantine-color-body)"
+            align="center"
+            justify="center"
+            gap="sm"
+            >
+                <Avatar src={user.avatar} size={150} radius={150} alt="it's me" />
+                <Text> {user.name}</Text>
+                <Text> {user.email}</Text>
+                <Text> {new Date(user.createdAt).toLocaleDateString()}</Text>
+            </Stack>
+        </Container>
+        </div>
+    )
+}
